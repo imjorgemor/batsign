5f1a487c4148ecbe811c8968e011b712ec14ac78
Jorge Mor <imgeorgemore@icloud.com>
2022-03-13 19:38:01 +0100

    Update font import from Index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import BatScreen from "./components/BatScreen";
+import WebFont from 'webfontloader';
 
 function App() {
+    WebFont.load({
+        google: {
+            families: ['Anton:400', 'sans-serif']
+        }
+    });
+
     return (
         <div className="app">
             <BatScreen />
